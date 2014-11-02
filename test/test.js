887bbf188cc3bc9de3a881a4105825973f412ef0
tillarnold <throwable42@gmail.com>
2014-11-02 17:24:12 +0100

    Improve unit tests

--- test/test.js
+++ test/test.js
@@ -24,5 +24,89 @@ test('Leinwand#circle', function(t) {
   var l = leinwand(mockCanvas);
 
   l.circle(10, 50, 55);
+});
+
+
+test('Leinwand#clear', function(t) {
+  t.plan(4);
+
+  var mockCanvas = {
+    getContext: function() {
+      return {
+        clearRect: function(x, y, w, h) {
+          t.equals(x, 0);
+          t.equals(y, 0);
+          t.equals(w, 999);
+          t.equals(h, 777);
+        }
+      };
+    },
+    width: 999,
+    height: 777
+  };
+  var l = leinwand(mockCanvas);
+
+  l.clear();
+});
+
+test('Leinwand#setters', function(t) {
+  t.plan(3);
+  var context = {};
+  var mockCanvas = {};
+  mockCanvas.getContext = function() {
+    return context;
+  };
+
+  var l = leinwand(mockCanvas);
+
+  l
+    .font(1)
+    .fillStyle(2)
+    .textAlign(3);
+
+  t.equals(context.font, 1);
+  t.equals(context.fillStyle, 2);
+  t.equals(context.textAlign, 3);
+
+});
+
+test('Leinwand#getters', function(t) {
+  t.plan(4);
+  var context = {};
+  var mockCanvas = {};
+  mockCanvas.getContext = function() {
+    return context;
+  };
+  mockCanvas.height = 200;
+  mockCanvas.width = 500;
+
+  var l = leinwand(mockCanvas);
+
+  t.equals(l.getCanvas(), mockCanvas);
+  t.equals(l.getContext(), context);
+  t.equals(l.getWidth(), 500);
+  t.equals(l.getHeight(), 200);
+
+});
+
+test('Leinwand#methods', function(t) {
+  t.plan(2);
+  var context = {};
+  context.save = function(e) {
+    t.equals(e, 2);
+  };
+  context.lineTo = function(e) {
+    t.equals(e, 1);
+  };
+  var mockCanvas = {};
+  mockCanvas.getContext = function() {
+    return context;
+  };
+
+  var l = leinwand(mockCanvas);
+
+  l
+    .lt(1)
+    .save(2);
 
 });
